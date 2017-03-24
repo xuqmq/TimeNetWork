82809f1c330e1e90738276b6fd6f5c885c5242c1
Administrator <582812876@qq.com>
2017-03-24 10:59:01 +0800

    第一次

---
+++ app/src/main/java/com/bowen/timenetwork/adapter/GuideAdapter.java
@@ -0,0 +1,39 @@
+package com.bowen.timenetwork.adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/3/23 0023.
+ */
+public class GuideAdapter extends PagerAdapter {
+    private List<View> list;
+    public  GuideAdapter(List<View> list){
+        this.list = list;
+    }
+    @Override
+    public int getCount() {
+        return list == null ?  0 : list.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    //添加子布局
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        container.addView(list.get(position));
+        return list.get(position);
+    }
+
+    //移除子布局
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(list.get(position));
+    }
+}
